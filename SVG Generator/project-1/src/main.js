367fe7920d519d09077a270ab6f3820bfe997e9d
Maybe-May-B <mayevelynboyer@gmail.com>
2024-03-20 01:25:01 -0400

    Project 1 Finished

--- SVG Generator/project-1/src/main.js
+++ SVG Generator/project-1/src/main.js
@@ -1,88 +1,60 @@
 import * as c from "./classes.js";
 import * as h from "./helper.js";
 
-let pathInstance,polylineInstance,translationInstance,rotationInstance,scalingInstance,styleInstance;
+let polylineInstance;
 const init = () => {
-    pathInstance = new c.Path();
     polylineInstance = new c.Polyline();
-    translationInstance = new c.Translation(100,100);
-    rotationInstance = new c.Rotation();
-    scalingInstance = new c.Scaling(100,100);
-    styleInstance = new c.Style({fill:"red",stroke:"red",'stroke-width':0});
-    drawContinent(100,10,10,10,{x:300,y:300},{x:100,y:100});
+    drawTiles(50,10,10,7,{x:100,y:100},{x:50,y:50});
+    
 }
 
-const drawContinent = (continentSides,countrySides,premadeCountries,amplitude,dimensions,margins) => {
-    let next = 0;
-    let continentPoints = polylineInstance.polygon(continentSides);
-    let countryPoints = [];
-    let connectingPoints = [];
-    let allPoints = [];
+const drawTiles = (sides,rows,columns,maxAmplitude,dimensions,margins) => {
+    let next;
     let elements = [];
-    let style = new c.Style({fill:'none',stroke:'white','stroke-width':5/Math.max(dimensions.x,dimensions.y)});
-    let translating = new c.Translation(margins.x,margins.y);
+    let style = new c.Style({fill:'none',stroke:'white','stroke-width':1/Math.max(dimensions.x,dimensions.y)});
     let scaling = new c.Scaling(dimensions.x,dimensions.y);
-    let xSkews = h.perlin(0,continentSides,amplitude/dimensions.x);
-    let ySkews = h.perlin(0,continentSides,amplitude/dimensions.y);
+    let rotations = h.perlin(0,rows*columns,maxAmplitude,0,360);
+    for(let y=0; y<rows;y++){
+        for(let x=0; x<columns;x++){
+            next = 0;
+            let points = polylineInstance.polygon(sides);
+            let translating = new c.Translation((dimensions.x+margins.x)*x,(dimensions.y+margins.y)*y);
+            let rotating = new c.Rotation(rotations[(y*columns)+x]);
+            let xSkews = h.perlin(0,sides,(maxAmplitude/(dimensions.x*columns))*(x+1));
+            let ySkews = h.perlin(0,sides,(maxAmplitude/(dimensions.y*rows))*(y+1));
+            for(let i=0; i< points.length; i++){
+                points[i].x += xSkews[i];
+                points[i].y += ySkews[i];
+            }
+            let shape = new c.Polyline(`shape`,[points[next]]);
+            points.splice(next,1);
+            while(shape.points.length < sides){
+                let current = shape.points[shape.points.length-1];
+                next = h.closestPoint(points,current);
+                shape.points.push(points[next]);
+                points.splice(next,1);
+            }
+            elements.push(new c.Group(`group-${x+1}-${y+1}`,style,[shape],[translating,rotating,scaling]));
+        }
+    }
     let body = document.querySelector("body");
-
-    for(let i=0; i< continentPoints.length; i++){
-        continentPoints[i].x += xSkews[i];
-        continentPoints[i].y += ySkews[i];
-    }
-
-    let continent = new c.Polyline("continent",[continentPoints[next]]);
-    continentPoints.splice(next,1);
-    while(continent.points.length < continentSides){
-        let current = continent.points[continent.points.length-1];
-        next = h.closestPoint(continentPoints,current);
-        continent.points.push(continentPoints[next]);
-        continentPoints.splice(next,1);
-    }
-    elements.push(continent);
-    allPoints = allPoints.concat(continent.points);
-
-    let xTranslations = h.perlin(0,premadeCountries,dimensions.x/premadeCountries);
-    let yTranslations = h.perlin(0,premadeCountries,dimensions.y/premadeCountries);
-    let countryScaling = new c.Scaling(1/premadeCountries);
-    for(let i = 0; i < premadeCountries; i++){
-        xSkews = h.perlin(0,countrySides,amplitude/(dimensions.x*2));
-        ySkews = h.perlin(0,countrySides,amplitude/(dimensions.y*2));
-        countryPoints.push(polylineInstance.polygon(countrySides));
-        for(let j=0; j < countrySides; j++){
-            countryPoints[i][j].x += xSkews[j];
-            countryPoints[i][j].y += ySkews[j];
-        }
-        next = 0;
-        let country = new c.Polyline(`country-${i+1}`,[countryPoints[i][next]]);
-        countryPoints[i].splice(next,1);
-        while(country.points.length < countrySides){
-            let current = country.points[country.points.length-1];
-            next = h.closestPoint(countryPoints[i],current);
-            country.points.push(countryPoints[i][next]);
-            countryPoints[i].splice(next,1);
-        }
-        let currentTranslation = new c.Translation(xTranslations[i],yTranslations[i]);
-        elements.push(new c.Group(`country-${i+1}`,style,[country],[currentTranslation,countryScaling]));
-    }
-    console.log(elements);
-    /*let xValues = [];
-    let yValues = [];
-    continent.points.forEach(element => {
-        xValues.push(element.x);
-        yValues.push(element.y);
-    });
-    for(let i=0; i<innerPoints; i++){
-        borderPoints.push({x:h.random(Math.min(...xValues),Math.max(...xValues)),
-            y:h.random(Math.min(...yValues),Math.max(...xValues))});
-    }
-    console.log(borderPoints);*/
-    let group = new c.Group("group-1",style,elements,[translating,scaling]);
+    let group = new c.Group("group-1",style,elements,[new c.Translation(margins.x,margins.y)]);
     body.innerHTML = `<svg id="svg-1" 
-        height="650" 
-        width="1000" style="background-color:black">
+        height="${rows*(dimensions.y+margins.y)+(margins.y*2)}" 
+        width="${columns*(dimensions.x+margins.x)+(margins.x*2)}" style="background-color:black">
             ${group.print()}
-        </svg>`;
+        </svg>
+        <button>save</button>`;
+    document.querySelector("button").onclick = download;
+}
+const download = () => {
+    console.log("check");
+    const file = new Blob([document.querySelector("#svg-1").outerHTML],{type: "image/svg+xml"});
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = "tiles.svg";
+    link.click();
+    URL.revokeObjectURL(link.href);
 }
 
 export {init};
